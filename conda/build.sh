30cced5e7637fe5673f3c09fa0f8c9403deb0eed
thraneh <thraneh@gmail.com>
2020-06-06 16:55:53 +0200

    make it compile

---
+++ conda/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+env
+
+export CPPFLAGS="-I$PREFIX/include -DNDEBUG -fvisibility=hidden $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+
+./autogen.sh
+
+./configure --prefix=$PREFIX
+
+make -j${CPU_COUNT:-2}
+
+make install-strip
