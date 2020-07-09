cd21d0a0ac32901e1f2a507a2e10f098e5f00c64
thraneh <thraneh@gmail.com>
2020-07-09 14:20:22 +0200

    adding cmake support

---
+++ test/stacktrace.cpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2020, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+#include <absl/debugging/stacktrace.h>
+
+TEST(stacktrace, simple) {
+  // XXX doesn't seem to work ...
+  void *addr[32];
+  int depth = absl::GetStackTrace(
+      addr,
+      std::size(addr),
+      0);
+  EXPECT_EQ(depth, 0);  // will fail when it works
+}
