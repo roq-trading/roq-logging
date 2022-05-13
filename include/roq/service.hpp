b49de11975ce3beae886fc85006c6722ea48822e
thraneh <thraneh@gmail.com>
2022-05-13 12:49:47 +0200

    revert defaults

--- include/roq/service.hpp
+++ include/roq/service.hpp
@@ -15,12 +15,12 @@ class ROQ_PUBLIC Service {
  public:
   struct Info final {
     std::string_view description;
-    std::string_view package_name;
-    std::string_view host;
-    std::string_view build_version;
-    std::string_view build_number;
-    std::string_view build_type;
-    std::string_view git_hash;
+    std::string_view package_name = {};
+    std::string_view host = {};
+    std::string_view build_version = {};
+    std::string_view build_number = {};
+    std::string_view build_type = {};
+    std::string_view git_hash = {};
     std::string_view compile_date = __DATE__;
     std::string_view compile_time = __TIME__;
   };
