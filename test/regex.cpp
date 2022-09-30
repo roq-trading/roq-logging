8af3528087d37109eea2a603ed8de067cab3e845
thraneh <thraneh@gmail.com>
2022-09-30 17:09:37 +0200

    drop c++14 support

--- test/regex.cpp
+++ test/regex.cpp
@@ -4,11 +4,7 @@
 
 #include <catch2/catch_all.hpp>
 
-#if (__cplusplus >= 201703L)
 #include <ctre.hpp>
-#else
-#include <regex>
-#endif
 
 using namespace std::literals;
 
@@ -17,7 +13,6 @@ const constexpr auto PATTERN = ctll::fixed_string{".*/opt/conda/.*work/(src/)?(.
 }
 
 TEST_CASE("regex_simple", "[regex]") {
-#if (__cplusplus >= 201703L)
   {  // "test 1"
     auto path = "/home/build/opt/conda/conda-bld/roq-logging_1641837162184/work/src/roq/flags/flags.cpp"sv;
     auto [whole, dummy, sub] = ctre::match<PATTERN>(path);
@@ -35,21 +30,4 @@ TEST_CASE("regex_simple", "[regex]") {
     auto [whole, dummy, sub] = ctre::match<PATTERN>(path);
     REQUIRE(whole == false);
   }
-#else
-  std::regex re(".*/opt/conda/.*work/(src/)?(.*)"s);
-  {  // "test 1"
-    auto path = "/home/build/opt/conda/conda-bld/roq-logging_1641837162184/work/src/roq/flags/flags.cpp"sv;
-    std::cmatch match;
-    std::regex_match(std::begin(path), std::end(path), match, re);
-    REQUIRE(std::size(match) == 3);
-    CHECK(match[2] == "roq/flags/flags.cpp"s);
-  }
-  {  // "test 2"
-    auto path = "/home/build/opt/conda/conda-bld/roq-oss-abseil-cpp_1641830430974/work/absl/flags/parse.cc"s;
-    std::smatch match;
-    std::regex_match(path, match, re);
-    REQUIRE(std::size(match) == 3);
-    CHECK(match[2] == "absl/flags/parse.cc");
-  }
-#endif
 }
