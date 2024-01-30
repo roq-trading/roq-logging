89b2fb5c0b6d9dd8fa1b349d521909453ccb5b3f
thraneh <thraneh@gmail.com>
2024-01-30 05:43:33 +0100

    use catch2 and ctre from conda-forge

--- src/roq/logging/test/regex.cpp
+++ src/roq/logging/test/regex.cpp
@@ -19,18 +19,18 @@ TEST_CASE("regex_simple", "[regex]") {
   {  // "test 1"
     auto path = "/home/build/opt/conda/conda-bld/roq-logging_1641837162184/work/src/roq/flags/flags.cpp"sv;
     auto [whole, dummy, sub] = ctre::match<PATTERN>(path);
-    REQUIRE(whole == true);
+    REQUIRE(!!whole == true);
     CHECK(sub == "roq/flags/flags.cpp"sv);
   }
   {  // "test 2"
     auto path = "/home/build/opt/conda/conda-bld/roq-oss-abseil-cpp_1641830430974/work/absl/flags/parse.cc"s;
     auto [whole, dummy, sub] = ctre::match<PATTERN>(path);
-    REQUIRE(whole == true);
+    REQUIRE(!!whole == true);
     CHECK(sub == "absl/flags/parse.cc"sv);
   }
   {  // "test 3"
     auto path = "parse.cc"s;
     auto [whole, dummy, sub] = ctre::match<PATTERN>(path);
-    REQUIRE(whole == false);
+    REQUIRE(!!whole == false);
   }
 }
