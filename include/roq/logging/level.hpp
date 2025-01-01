3182081880bd37569f7891cbd12c3bc095aea4d0
thraneh <thraneh@gmail.com>
2025-01-01 08:38:43 +0100

    fix macos

--- include/roq/logging/level.hpp
+++ include/roq/logging/level.hpp
@@ -19,12 +19,3 @@ enum class Level {
 
 }  // namespace logging
 }  // namespace roq
-
-template <>
-struct fmt::formatter<roq::logging::Level> {
-  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
-  auto format(roq::logging::Level const &value, format_context &context) const {
-    using namespace std::literals;
-    return fmt::format_to(context.out(), "{}"sv, value);
-  }
-};
